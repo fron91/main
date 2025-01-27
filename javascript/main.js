480579de0e3806789a21c075c7f29f2694a93406
fron86 <fronmustafa2012@gmail.com>
2025-01-27 03:18:44 -0800

    ,
    final

---
+++ javascript/main.js
@@ -0,0 +1,53 @@
+console.log("Goodbye");
+
+var x=5;
+console.log(a);
+
+var b=20;
+var c=30;
+var d= b+c;
+console.log(d);
+
+var e=20;
+var f=30;
+var g= e+f;
+console.log(g);
+
+var h=100;
+var i=5;
+var j= h+i;
+console.log(j);
+
+var k=10;
+var l=3;
+var m= k % l;
+console.log(m);
+
+var n=99;
+n++;
+console.log(n);
+
+var o=99;
+0--;
+console.log(n);
+
+var p=20;
+var q=p**2;
+console.log(q)
+
+var x= 15;
+x +=5;
+console.log(x);
+
+
+var s=10;
+s *=7;
+console.log(s)
+
+var v=5;
+var w=5;
+console.log(v == w);
+
+var z
+
+
